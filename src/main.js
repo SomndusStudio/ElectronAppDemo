88cb8575e0aee639a874620a8a6212107f97f622
SomndusStudio <74624954+SomndusStudio@users.noreply.github.com>
2025-09-26 00:34:33 +0200

    First commit

---
+++ src/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron/main')
+const path = require('node:path')
+
+const { updateElectronApp } = require('update-electron-app');
+updateElectronApp();
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'src/preload.js')
+        }
+    })
+
+    win.loadFile('src/index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
